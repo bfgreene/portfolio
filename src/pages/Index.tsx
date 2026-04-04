6e14dd1cbc410c46f8ca7e11ce1c5fcd3f8fd7cf
Ben Greene <bfgreene@gmail.com>
2026-04-03 20:07:35 -0700

    Update Index.tsx
    changed header title

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -65,7 +65,7 @@ const Index = () => {
 
         {/* Misc */}
         <section className="mb-8">
-          <h2 className="text-2xl md:text-3xl mb-1">Miscellaneous</h2>
+          <h2 className="text-2xl md:text-3xl mb-1">Misc.</h2>
           <p className="text-sm text-muted-foreground mb-6 italic">
             Process, details, and other work.
           </p>
