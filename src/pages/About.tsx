54e6a6512918330d0b9466755a151bd3e75a5aee
Ben Greene <bfgreene@gmail.com>
2026-05-05 16:35:54 -0700

    Remove commented-out code in About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -91,8 +91,8 @@ const About = () => {
                 Benjamin Greene (IV)
               </a>
             </li>
-            */}
           </ul>
+          */}
         </section>
     
         {/* CV */}
